3b47233957faeb9b54d1a36698317e95517b24e7
OTR <49165533+OTR@users.noreply.github.com>
2021-12-20 12:48:34 +0300

    [CHORE] Slightly clean the code

--- python/console_game/console_mud_game.py
+++ python/console_game/console_mud_game.py
@@ -1,40 +1,44 @@
 """
-ccaccabc
 This is some of my very first python programs
 """
 import shelve
 import sys
 from random import randint, choice
 
-
 __version__ = '0.0.3'
-commands = ["n",  # go to north
-            "s",  # go to south
-            "w",  # go to west
-            "e",  # go to east
-            "p",  # pick up resources
-            "exit",  # exit from game
-            "randturn"]  # rand location
+COMMANDS = {
+    "n": "go to north",
+    "s": "go to south",
+    "w": "go to west",
+    "e": "go to east",
+    "p": "pick up resources",
+    "exit": "exit from game",
+    "rand_move": "make random move"
+}
 
-location_first_name = ['Бурая', 'Смертельная', 'Счастливая', 'Дикая',
-                       'Кровавая', 'Зеленая', 'Верхняя', 'Заколдованная',
-                       'Древняя']
+LOCATION_FIRST_NAME = [
+    'Бурая', 'Смертельная', 'Счастливая', 'Дикая', 'Кровавая', 'Зеленая',
+    'Верхняя', 'Заколдованная', 'Древняя'
+]
 
-location_second_name = ['Роща', 'Поляна', 'Побережье', 'Овраг', 'Лес',
-                        'Пустыня', 'Горы', 'Озеро', 'Болото', 'Ущелье', 'Оазис']
+LOCATION_SECOND_NAME = [
+    'Роща', 'Поляна', 'Побережье', 'Овраг', 'Лес', 'Пустыня', 'Горы', 'Озеро',
+    'Болото', 'Ущелье', 'Оазис'
+]
 
 
 class BasicLocation(object):
     """"""
+
     def __init__(self, resource=None, name=None):
         """"""
-        self.resource = resource
+        self.resource = resource  # FIXME:
         if name:
             self.name = name
         else:
             self.name = 'Тестовая локация #%d' % randint(0, 255)
 
-    def sub_resource(self):
+    def subtract_resource(self):
         """"""
         if self.resource:
             self.resource -= 1
@@ -44,23 +48,23 @@ class BasicLocation(object):
 
     def add_resource(self):
         """Generate new resource."""
-        pass
+        raise NotImplementedError
 
     def link(self):
-        """Link a location to another, make portal."""
-        pass
+        """Link a location to another, make a portal."""
+        raise NotImplementedError
 
 
 class ShelveLocation(BasicLocation):
     """"""
 
-    def __init__(self, link_db, loc, resource=None, name=None):
+    def __init__(self, link_db, location, resource=None, name=None):
         """"""
         BasicLocation.__init__(self, resource, name)
         self.link_db = link_db
-        self.loc = loc
+        self.location = location
 
-    def sub_resource(self):
+    def subtract_resource(self):
         """"""
         if self.resource:
             self.resource -= 1
@@ -71,111 +75,114 @@ class ShelveLocation(BasicLocation):
 
     def transfer(self):
         """"""
-        tmp = self.link_db[self.loc]
+        tmp = self.link_db[self.location]
         tmp[1] = self.resource
-        self.link_db[self.loc] = tmp
+        self.link_db[self.location] = tmp
 
 
 class ShelveDB(object):
     """"""
+
     def __init__(self, radius, base=None):
         """"""
         if base:
-            self.db = shelve.open(base)
+            self.storage = shelve.open(base)
         else:
-            self.db = shelve.open('./db.dat')
+            self.storage = shelve.open('./db.dat')
         self.radius = radius
         self.prefix = len(str(radius * 2))
-        self.sl = self.make_start_location()
-        self.db.sync()
+        self.start_location = self.make_start_location()
+        self.storage.sync()
 
     def make_dungeon(self):
         """Make a dungeon."""
         pass
 
-    def make_start_location(self, sl=None):
+    def make_start_location(self, start_location=None):
         """"""
-        if sl is None:
+        if start_location is None:
             _null = '0' * self.prefix
-            sl = '%s:%s' % (_null, _null)
-        else:
-            pass  # Not standard start location
-        name = "Спуск в подземелье"
-        resource = randint(0, 255)
-        self.db[sl] = [name, resource]  # lw=None,ln=None,ls=None,le=None]
+            start_location = '%s:%s' % (_null, _null)
+        location_name = "Спуск в подземелье"
+        resource_amount = randint(0, 255)
+        # lw=None,ln=None,ls=None,le=None]
         # lw - link to west location etc
-        return sl
+        self.storage[start_location] = [location_name, resource_amount]
+
+        return start_location
 
 
-def load_rand_locate():
+def make_rand_location():
     """Для режима без карты вообще."""
-    global location_first_name, location_second_name
-    name = "%s %s" % (choice(location_first_name), choice(location_second_name))
+    name = "%s %s" % (choice(LOCATION_FIRST_NAME), choice(LOCATION_SECOND_NAME))
     location = BasicLocation(resource=randint(0, 5), name=name)
+
     return location
 
 
-def generate_world(radius):
-    """проверка на существование базы"""
+def load_game_world_from_db(radius):
+    """Try load base from a file."""
     try:
-        pass
-    except:
-        pass
-    world = ShelveDB(radius)
-    return world
+        world = ShelveDB(radius)
+    except OSError as err:
+        raise err
+    else:
+        return world
 
 
 def render_pseudo_graphic(name, res, turn):
     """Print pseudo graphic interface in console."""
-
-    def rev_str(string):
-        """Reverse word in string."""
-        # ''.join([x for x in decor].reverse())
-        return ''.join(reversed(list(string)))
-
-    _line = '#' * 34 + (' TURN #%3d ' % turn) + '#' * 35
+    title_line = '#' * 34 + (' TURN #%3d ' % turn) + '#' * 35
     decor = ',~~~ @ '
-    letters = {'input_lc': 'Вы находитесь в локации :',
-               'input_res': 'Кол-во ресурсов %d шт.'}
-    # dec_name = '{0:{fill}{align}80}'.format((decor+name+rev_str(decor)),fill = ' ',align = '^')
-    dec_name = (decor + name + rev_str(decor)).center(80)
-    print(_line)
-    print(letters['input_lc'])
-    print(dec_name)
-    print(letters['input_res'] % res)
+    reversed_decor = decor[::-1]
+    location_description = {
+        'curr_location': 'Вы находитесь в локации :',
+        'curr_res_amount': 'Кол-во ресурсов %d шт.'
+    }
+    # decorated_location_name = '{0:{fill}{align}80}'.format(
+    # (decor + name + rev_str(decor)), fill=' ', align='^')
+
+    decorated_location_name = f"{decor}{name}{reversed_decor}".center(80)
+    print(title_line)
+    print(location_description['curr_location'])
+    print(decorated_location_name)
+    print(location_description['curr_res_amount'] % res)
 
 
 def main():
     """"""
-    db = generate_world(18)
-    world = db.db
-    sl = db.sl
-    loc = ShelveLocation(world, sl, world[sl][1], world[sl][0])
-    # loc = load_rand_locate()
-    res, name = loc.resource, loc.name
+    shelve_object = load_game_world_from_db(18)
+    storage = shelve_object.storage
+    start_location = shelve_object.start_location
+    location = ShelveLocation(
+        storage,
+        start_location,
+        storage[start_location][1],
+        storage[start_location][0]
+    )
+    # location = load_rand_location()
+    res, name = location.resource, location.name
     turn = 0
     while True:
         render_pseudo_graphic(name, res, turn)
         command = input("Сделать ход: ")
-        if command in commands:
+        if command in COMMANDS:
             if command == "exit":
                 print('#' * 80)
-                print(world.sync())
+                storage.sync()
                 sys.exit()
-            # elif command == "randturn":
-            #   loc = load_rand_locate()
-            #   res, name = loc.resource, loc.name
+            # elif command == "rand_move":
+            #   location = load_rand_locate()
+            #   res, name = location.resource, location.name
             elif command == "p":
-                get = loc.sub_resource()
-                if get:
-                    print("Вы собрали %d ресурса" % get)
-                    res = loc.resource
+                is_picked = location.subtract_resource()
+                if is_picked:
+                    print("Вы собрали %d ресурса" % is_picked)
+                    res = location.resource
                 else:
                     print("Вы ничего не собрали")
             turn += 1  # Изменять шаг только после выполнения команды
-        #  в противном случае мы ничего не сделали
-        else:
-            pass
+
         print('#' * 80)
 
 
--- python/console_game/game_with_curses.py
+++ python/console_game/game_with_curses.py
@@ -1,8 +1,9 @@
 """
-acaicabb
 This is some of my very first python programs
 """
 import curses
+import sys
+from collections import namedtuple
 
 
 GAME_OBJECTS = []
@@ -25,135 +26,157 @@ BORDER = '''##################
 #                #
 ##################'''
 
+GAME_COLOR = namedtuple("CursesColor", "num_pair fg_color bg_color")
 
-def draw_border():
+
+class GameBoard(object):
     """"""
-    myscreen.addstr(0, 0, BORDER)
-    myscreen.addstr(21, 2, "Lv:%s | Eq:%s | Hit:%s" % ("1", "S.Axe", "50/50"))
+    BLACK_COLOR = GAME_COLOR(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    MAGENTA_COLOR = GAME_COLOR(2, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+    RED_COLOR = GAME_COLOR(3, curses.COLOR_RED, curses.COLOR_RED)
 
-
-def move_character(key):
-    """"""
-    myscreen.clear()
-    draw_border()
-    if key == "down":
-        hero.down()
-    elif key == "up":
-        hero.up()
-    elif key == "left":
-        hero.left()
-    elif key == "right":
-        hero.right()
-    else:
-        print("Error in Move()")
-    myscreen.refresh()
-
-
-class Character(object):
-    """"""
-
-    def __init__(self, screen, start_pos, avatar):
+    def __init__(self, start_pos, avatar):
         """"""
-        self.screen = screen
+        self.screen = curses.initscr()
+        self.initiate_curses_colors()
         self.start_pos = start_pos
         self.pos = start_pos
         self.avatar = avatar
         self.action = []
 
+    @staticmethod
+    def initiate_curses_colors():
+        """"""
+        curses.start_color()
+        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+        curses.init_pair(2, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+        curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
+
     def render(self, pos=None):
         """"""
         if pos is None:
             pos = [1, 1]
-        if pos != [1, 1]:
-            self.screen.addstr(pos[0], pos[1], self.avatar,
-                               curses.color_pair(2)
-                               )
-            self.pos = pos
+
+        if pos == [1, 1]:
+            self.screen.addstr(
+                self.start_pos[0], self.start_pos[1], self.avatar,
+                curses.color_pair(2)
+            )
         else:
-            self.screen.addstr(self.start_pos[0], self.start_pos[1],
-                               self.avatar)
+            self.screen.addstr(
+                pos[0], pos[1], self.avatar, curses.color_pair(2)
+            )
+            self.pos = pos
 
     def down(self):
         """"""
         if self.pos[0] < 16:
             self.pos = [self.pos[0] + 1, self.pos[1]]
-        else:
-            pass
+
         self.render(self.pos)
         self.action = [self.pos[0] + 1, self.pos[1]]
-        self.screen.chgat(self.action[0], self.action[1], 1,
-                          curses.color_pair(1))
+        self.screen.chgat(
+            self.action[0], self.action[1], 1,curses.color_pair(1)
+        )
 
     def up(self):
         """"""
         if self.pos[0] > 1:
             self.pos = [self.pos[0] - 1, self.pos[1]]
-        else:
-            pass
+
         self.render(self.pos)
         self.action = [self.pos[0] - 1, self.pos[1]]
-        self.screen.chgat(self.action[0], self.action[1], 1,
-                          curses.color_pair(1))
+        self.screen.chgat(
+            self.action[0], self.action[1], 1, curses.color_pair(1)
+        )
 
     def left(self):
         """"""
         if self.pos[1] > 1:
             self.pos = [self.pos[0], self.pos[1] - 1]
-        else:
-            pass
+
         self.render(self.pos)
         self.action = [self.pos[0], self.pos[1] - 1]
-        self.screen.chgat(self.action[0], self.action[1], 1,
-                          curses.color_pair(1))
+        self.screen.chgat(
+            self.action[0], self.action[1], 1, curses.color_pair(1)
+        )
 
     def right(self):
         """"""
         if self.pos[1] < 16:
             self.pos = [self.pos[0], self.pos[1] + 1]
-        else:
-            pass
+
         self.render(self.pos)
         self.action = [self.pos[0], self.pos[1] + 1]
-        self.screen.chgat(self.action[0], self.action[1], 1,
-                          curses.color_pair(1))
+        self.screen.chgat(
+            self.action[0], self.action[1], 1, curses.color_pair(1)
+        )
 
     def dig(self):
         """"""
-        self.screen.addstr(self.action[0], self.action[1], "T",
-                           curses.color_pair(3))
+        self.screen.addstr(
+            self.action[0], self.action[1], "T", curses.color_pair(3)
+        )
         GAME_OBJECTS.append((self.action[0], self.action[1], "T"))
 
+    def draw_border(self):
+        """"""
+        self.screen.addstr(0, 0, BORDER)
+        self.screen.addstr(
+            21, 2, "Lv:%s | Eq:%s | Hit:%s" % ("1", "S.Axe", "50/50")
+        )
 
-if __name__ == '__main__':
+    def move_character(self, key):
+        """"""
+        self.screen.clear()
+        self.draw_border()
+        if key == "down":
+            self.down()
+        elif key == "up":
+            self.up()
+        elif key == "left":
+            self.left()
+        elif key == "right":
+            self.right()
+        else:
+            print("Error in Move()")
+        self.screen.refresh()
+
+
+def main() -> None:
+    """"""
     try:
-        myscreen = curses.initscr()
-        curses.start_color()
-        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
-        curses.init_pair(2, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
-        curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
-        draw_border()
-        hero = Character(myscreen, [1, 1], "@")
-        hero.render()
-        myscreen.refresh()
+        player_start_pos = (1, 1)
+        player_character = "@"
+        game = GameBoard(player_start_pos, player_character)
+        game.draw_border()
+        game.render()
+        game.screen.refresh()
         while True:
-            key = myscreen.getch(21, 0)
+            key = game.screen.getch(21, 0)
             if key == 115:
-                move_character("down")
+                game.move_character("down")
             elif key == 119:
-                move_character("up")
+                game.move_character("up")
             elif key == 97:
-                move_character("left")
+                game.move_character("left")
             elif key == 100:
-                move_character("right")
+                game.move_character("right")
             elif key == 103:
-                hero.dig()
+                game.dig()
             elif key == 121:
-                myscreen.clear()
+                game.screen.clear()
                 print(GAME_OBJECTS)
             else:
                 pass
             if GAME_OBJECTS:
                 for obj in GAME_OBJECTS:
-                    myscreen.addstr(obj[0], obj[1], obj[2])
+                    game.screen.addstr(obj[0], obj[1], obj[2])
+    except KeyboardInterrupt as err:
+        sys.exit()
     finally:
         curses.endwin()
+
+
+if __name__ == '__main__':
+    main()
