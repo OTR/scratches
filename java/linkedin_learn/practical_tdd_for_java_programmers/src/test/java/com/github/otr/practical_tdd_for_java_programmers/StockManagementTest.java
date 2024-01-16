3a268d8f08b83444d11b61c2fd4771130a649836
OTR <49165533+OTR@users.noreply.github.com>
2024-01-16 10:54:00 +0600

    [TEST] Implement tests for calling database exactly once

--- java/linkedin_learn/practical_tdd_for_java_programmers/src/test/java/com/github/otr/practical_tdd_for_java_programmers/StockManagementTest.java
+++ java/linkedin_learn/practical_tdd_for_java_programmers/src/test/java/com/github/otr/practical_tdd_for_java_programmers/StockManagementTest.java
@@ -8,7 +8,12 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.equalTo;
 
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  *
@@ -67,19 +72,23 @@ public class StockManagementTest {
         ExternalService mockedDatabaseService = mock(ExternalService.class);
         ExternalService mockedWebService = mock(ExternalService.class);
 
+        String expected = "6489" + "D" + "5";
+        String input = CORRECT_13_ISBN_NUMBER;
+
+        Book invalidBook = new Book(input, "Abc", "Abc");
+
+        when(mockedDatabaseService.lookup(input)).thenReturn(invalidBook);
+
         StockManager stockManager = new StockManager();
         stockManager.setWebService(mockedWebService);
         stockManager.setDatabaseService(mockedDatabaseService);
-
-        String expected = "6489" + "D" + "5";
-        String input = CORRECT_13_ISBN_NUMBER;
 
         // WHEN
         String actual = stockManager.getLocatorCode(input);
 
         // THEN
-        assertThat(expected, equalTo(actual));
-
+        verify(mockedDatabaseService, times(1)).lookup(input);
+        verify(mockedWebService, never()).lookup(anyString());
 
     }
 
@@ -88,6 +97,29 @@ public class StockManagementTest {
      */
     @Test
     public void webServiceIsUsedIfDataIsNotPresentInDatabase() {
+
+        // GIVEN
+        // Creates a dummy class that implements given interface
+        ExternalService mockedDatabaseService = mock(ExternalService.class);
+        ExternalService mockedWebService = mock(ExternalService.class);
+
+        String expected = "6489" + "D" + "5";
+        String input = CORRECT_13_ISBN_NUMBER;
+        Book someInvalidBook = new Book(input, "Abc", "Abc");
+
+        when(mockedDatabaseService.lookup(input)).thenReturn(null);
+        when(mockedWebService.lookup(input)).thenReturn(someInvalidBook);
+
+        StockManager stockManager = new StockManager();
+        stockManager.setWebService(mockedWebService);
+        stockManager.setDatabaseService(mockedDatabaseService);
+
+        // WHEN
+        String actual = stockManager.getLocatorCode(input);
+
+        // THEN
+        verify(mockedDatabaseService).lookup(input); // exactly once
+        verify(mockedWebService, times(1)).lookup(input);
 
     }
 
