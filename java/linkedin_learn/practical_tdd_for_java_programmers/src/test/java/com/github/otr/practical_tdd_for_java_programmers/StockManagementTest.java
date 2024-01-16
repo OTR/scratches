d6b734e2dcfa8bc342201722dbfd3aa6652e9f5f
OTR <49165533+OTR@users.noreply.github.com>
2024-01-16 09:36:45 +0600

    [FEAT] Instantiate a mock versions of web service and database service

--- java/linkedin_learn/practical_tdd_for_java_programmers/src/test/java/com/github/otr/practical_tdd_for_java_programmers/StockManagementTest.java
+++ java/linkedin_learn/practical_tdd_for_java_programmers/src/test/java/com/github/otr/practical_tdd_for_java_programmers/StockManagementTest.java
@@ -2,10 +2,13 @@ package com.github.otr.practical_tdd_for_java_programmers;
 
 import com.github.otr.practical_tdd_for_java_programmers.domain.Book;
 import com.github.otr.practical_tdd_for_java_programmers.domain.ExternalService;
+
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.equalTo;
+
+import static org.mockito.Mockito.mock;
 
 /**
  *
@@ -58,6 +61,25 @@ public class StockManagementTest {
      */
     @Test
     public void testDatabaseIsUsedIfDataIsPresent() {
+
+        // GIVEN
+        // Creates a dummy class that implements given interface
+        ExternalService mockedDatabaseService = mock(ExternalService.class);
+        ExternalService mockedWebService = mock(ExternalService.class);
+
+        StockManager stockManager = new StockManager();
+        stockManager.setWebService(mockedWebService);
+        stockManager.setDatabaseService(mockedDatabaseService);
+
+        String expected = "6489" + "D" + "5";
+        String input = CORRECT_13_ISBN_NUMBER;
+
+        // WHEN
+        String actual = stockManager.getLocatorCode(input);
+
+        // THEN
+        assertThat(expected, equalTo(actual));
+
 
     }
 
