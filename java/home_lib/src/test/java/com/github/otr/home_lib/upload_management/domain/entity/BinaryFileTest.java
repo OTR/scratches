639458cb09db426bee7ddeb83cdeec925ea78559
OTR <49165533+OTR@users.noreply.github.com>
2024-01-21 08:22:30 +0600

    [TEST] Write a simple failing test

---
+++ java/home_lib/src/test/java/com/github/otr/home_lib/upload_management/domain/entity/BinaryFileTest.java
@@ -0,0 +1,82 @@
+package com.github.otr.home_lib.upload_management.domain.entity;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertThrows;
+
+/**
+ *
+ */
+public class BinaryFileTest {
+
+    /**
+     *
+     */
+    @Test
+    public void constructor_setsFilenameAndFileContentCorrectly() {
+        String filename = "test.epub";
+        byte[] fileContent = new byte[] {1, 2, 3};
+        BinaryFile file = new BinaryFile(filename, fileContent);
+
+        assertEquals(filename, file.getFilename());
+        assertArrayEquals(fileContent, file.getFileContent());
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void constructor_generatesUUIDBasedOnFileContent() {
+        byte[] some = new byte[] {1, 2, 3};
+        byte[] same = new byte[] {1, 2, 3};
+        byte[] another = new byte[] {4, 5, 6};
+
+        BinaryFile someF = new BinaryFile("file1.epub", some);
+        BinaryFile sameF = new BinaryFile("file2.epub", same);
+        BinaryFile anotherF = new BinaryFile("file3.epub", another);
+
+        // Same UUID for identical content
+        assertEquals(someF.getId(), sameF.getId());
+        // Different UUIDs for different content
+        assertNotEquals(someF.getId(), anotherF.getId());
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void constructor_throwsExceptionForEmptyFilename() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new BinaryFile("", new byte[] {})
+        );
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void constructor_throwsExceptionForNullFileContent() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new BinaryFile("test.epub", null)
+        );
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void getters_returnExpectedValues() {
+        String filename = "test.epub";
+        byte[] fileContent = new byte[] {1, 2, 3};
+        BinaryFile file = new BinaryFile(filename, fileContent);
+
+        assertEquals(filename, file.getFilename());
+        assertArrayEquals(fileContent, file.getFileContent());
+    }
+
+}
