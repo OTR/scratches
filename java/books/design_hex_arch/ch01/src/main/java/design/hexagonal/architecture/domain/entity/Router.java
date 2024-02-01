665a0c2b4efa727143bb9cbb1ea61ceb92f3d53c
OTR <49165533+otr@users.noreply.github.com>
2024-02-01 18:05:28 +0600

    [FEAT] Fill in the missing in the book code for Router domain entity

--- java/books/design_hex_arch/ch01/src/main/java/design/hexagonal/architecture/domain/entity/Router.java
+++ java/books/design_hex_arch/ch01/src/main/java/design/hexagonal/architecture/domain/entity/Router.java
@@ -7,34 +7,63 @@ import design.hexagonal.architecture.domain.vo.RouterType;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Router {
 
-    private final RouterId id;
-    private final RouterType type;
+    private final RouterId routerId;
+    private final RouterType routerType;
 
-    public Router(RouterType type, RouterId id) {
-        this.type = type;
-        this.id = id;
+    public Router(RouterType routerType, RouterId routerId) {
+        this.routerType = routerType;
+        this.routerId = routerId;
     }
 
-    public static List<Router> checkRouter(
-        RouterType type, List<Router> routers
-    ) {
-        var routersList = new ArrayList<Router>();
-        routers.forEach(router -> {
-            if (router.type.equals(type)) {
-                routersList.add(router);
-            }
-        });
+    public static Predicate<Router> filterRouterByType(RouterType routerType) {
+        return routerType.equals(RouterType.CORE) ? isCore() : isEdge();
+    }
 
-        return routersList;
+    private static Predicate<Router> isCore() {
+        return p -> p.getRouterType() == RouterType.CORE;
+    }
+
+    private static Predicate<Router> isEdge() {
+        return p -> p.getRouterType() == RouterType.EDGE;
     }
 
     public static List<Router> retrieveRouter(
             List<Router> routers,
-            Predicate<Router> filter
+            Predicate<Router> predicate
     ) {
-        throw new NotImplementedYet();
+        return routers.stream()
+                .filter(predicate)
+                .collect(Collectors.<Router>toList());
     }
+
+    private RouterType getRouterType() {
+        return this.routerType;
+    }
+
+    @Override
+    public String toString() {
+        return "Router{" +
+                "routerType=" + routerType +
+                ", routerId=" + routerId +
+                "}";
+    }
+
+//      DEPRECATED
+//    public static List<Router> checkRouter(
+//            RouterType type, List<Router> routers
+//    ) {
+//        var routersList = new ArrayList<Router>();
+//        routers.forEach(router -> {
+//            if (router.routerType.equals(type)) {
+//                routersList.add(router);
+//            }
+//        });
+//
+//        return routersList;
+//    }
+
 }
