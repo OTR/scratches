6d953f692410dafc3e056b413cf68df9cc81f0e1
OTR <49165533+otr@users.noreply.github.com>
2024-02-01 21:41:48 +0600

    [FEAT] Implement value objects for parsing Network Activity logs

---
+++ java/books/design_hex_arch/ch01/src/main/java/design/hexagonal/architecture/domain/vo/EventId.java
@@ -0,0 +1,11 @@
+package design.hexagonal.architecture.domain.vo;
+
+import design.hexagonal.architecture.domain.exception.NotImplementedYet;
+
+public class EventId {
+
+    public EventId() {
+        throw new NotImplementedYet();
+    }
+
+}
--- java/books/design_hex_arch/ch01/src/main/java/design/hexagonal/architecture/domain/vo/RouterId.java
+++ java/books/design_hex_arch/ch01/src/main/java/design/hexagonal/architecture/domain/vo/RouterId.java
@@ -1,15 +1,23 @@
 package design.hexagonal.architecture.domain.vo;
 
+import design.hexagonal.architecture.domain.exception.NotImplementedYet;
+
+import java.util.UUID;
+
 public class RouterId {
 
-    private String id;
+    private final UUID id;
 
-    private RouterId(String id) {
+    private RouterId(UUID id) {
         this.id = id;
     }
 
-    public static RouterId of(String id) {
-        return new RouterId(id);
+    public static RouterId withId(String id) {
+        return new RouterId(UUID.fromString(id));
+    }
+
+    public static RouterId withoutId() {
+        return new RouterId(UUID.randomUUID());
     }
 
     @Override
