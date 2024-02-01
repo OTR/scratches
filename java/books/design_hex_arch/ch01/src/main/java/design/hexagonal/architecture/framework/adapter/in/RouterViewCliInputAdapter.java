06a06fb9761f6fbeeee2a843b0f08388c4420118
OTR <49165533+otr@users.noreply.github.com>
2024-02-01 18:24:55 +0600

    [CHORE] Correct mismatch between github repo and source code from the book

--- java/books/design_hex_arch/ch01/src/main/java/design/hexagonal/architecture/framework/adapter/in/RouterViewCliInputAdapter.java
+++ java/books/design_hex_arch/ch01/src/main/java/design/hexagonal/architecture/framework/adapter/in/RouterViewCliInputAdapter.java
@@ -3,6 +3,7 @@ package design.hexagonal.architecture.framework.adapter.in;
 import design.hexagonal.architecture.application.use_case.RouterViewUseCase;
 import design.hexagonal.architecture.application.port.in.RouterViewInputPort;
 import design.hexagonal.architecture.domain.entity.Router;
+import design.hexagonal.architecture.domain.vo.RouterType;
 import design.hexagonal.architecture.framework.adapter.out.RouterViewFileOutputAdapter;
 
 import java.util.List;
@@ -16,10 +17,9 @@ public class RouterViewCliInputAdapter {
     }
 
     public List<Router> obtainRelatedRouters(String type) {
-        RelatedRoutersCommand relatedRoutersCommand =
-            new RelatedRoutersCommand(type);
-
-        return routerViewUseCase.getRelatedRouters(relatedRoutersCommand);
+        return routerViewUseCase.getRouters(
+                Router.filterRouterByType(RouterType.valueOf(type))
+        );
     }
 
     private void setAdapters() {
