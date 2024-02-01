a5134ec5623b37631129709a02d8eecc1344c460
OTR <49165533+otr@users.noreply.github.com>
2024-02-01 18:46:30 +0600

    [FEAT] Implement Entry poin class to launch CLI Adapter

---
+++ java/books/design_hex_arch/ch01/src/main/java/design/hexagonal/architecture/App.java
@@ -0,0 +1,24 @@
+package design.hexagonal.architecture;
+
+import design.hexagonal.architecture.domain.entity.Router;
+import design.hexagonal.architecture.framework.adapter.in.RouterViewCliInputAdapter;
+
+import java.util.List;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class App {
+
+    public static void main(String[] args) {
+        var cli = new RouterViewCliInputAdapter();
+        String type = "CORE";
+        List<Router> routers = cli.obtainRelatedRouters(type);
+        System.out.println(
+                routers.stream()
+                    .map(Router::toString)
+                    .collect(Collectors.joining("\n"))
+        );
+    }
+
+}
--- java/books/design_hex_arch/ch01/src/main/java/design/hexagonal/architecture/Main.java
+++
@@ -1,9 +0,0 @@
-package design.hexagonal.architecture;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello!");
-    }
-
-}
