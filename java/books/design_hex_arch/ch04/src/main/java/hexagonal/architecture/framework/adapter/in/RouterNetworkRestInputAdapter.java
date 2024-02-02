80f9eb1ed0e54ca86a8a8c05f16502fd07020317
OTR <49165533+otr@users.noreply.github.com>
2024-02-02 15:48:45 +0600

    [WIP][FEAT] Partly implement HTTP REST Input Adapter

---
+++ java/books/design_hex_arch/ch04/src/main/java/hexagonal/architecture/framework/adapter/in/RouterNetworkRestInputAdapter.java
@@ -0,0 +1,58 @@
+package hexagonal.architecture.framework.adapter.in;
+
+import hexagonal.architecture.application.use_case.RouterNetworkUseCase;
+import hexagonal.architecture.domain.entity.Router;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RouterNetworkRestInputAdapter extends RouterNetworkInputAdapter {
+
+    private final RouterNetworkUseCase routerNetworkUseCase;
+
+    public RouterNetworkRestInputAdapter(RouterNetworkUseCase useCase) {
+        this.routerNetworkUseCase = useCase;
+    }
+
+    @Override
+    public Router processRequest(Object requestParams) {
+        Map<String, String> params = new HashMap<>();
+
+        if (requestParams instanceof HttpServer) {
+            var httpserver = (HttpServer) requestParams;
+
+            httpserver.createContext("/network/add", (exchange -> {
+                if ("GET".equals(exchange.getRequestMethod())) {
+                    var query = exchange.getRequestURI().getRawQuery();
+                    httpParams(query, params);
+                    router = this.addNetworkToRouter(params);
+                    ObjectMapper mapper = new ObjectMapper();
+                    Json routerJson = mapper.writeValueAsString(
+                        RouterJsonFileMapper.toJson(router)
+                    );
+                    exchange.getResponseHeaders()
+                        .set("Content-Type", "application/json");
+                    exchange.sendResponseHeaders(
+                        200, routerJson.getBytes().length
+                    );
+                    OutputStream output = exchange.getResponseBody();
+                    output.write(routerJson.getBytes());
+                    output.flush();
+                } else {
+                    exchange.sendResponseHeaders(405, -1);
+                }
+
+                exchange.close();
+            }));
+
+            httpserver.setExecutor(null);
+            httpserver.start();
+        }
+        return router;
+    }
+
+}
