dc47a424e9354acdf381a047841d8135fdab88ed
OTR <49165533+OTR@users.noreply.github.com>
2024-02-03 13:35:54 +0600

    [FIX] Add constructor to Same IP Spec

--- java/books/design_hex_arch/ch06/topology-inventory/domain/src/main/java/hex/arch/topologyinventory/domain/spec/EmptySwitchSpecification.java
+++ java/books/design_hex_arch/ch06/topology-inventory/domain/src/main/java/hex/arch/topologyinventory/domain/spec/EmptySwitchSpecification.java
@@ -1,7 +1,6 @@
 package hex.arch.topologyinventory.domain.spec;
 
 import hex.arch.topologyinventory.domain.entity.EdgeRouter;
-import hex.arch.topologyinventory.domain.entity.Router;
 
 public class EmptySwitchSpecification implements Specification<EdgeRouter> {
 
