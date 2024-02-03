6309821ec66f7a19a0042d727419594b5da51b8c
OTR <49165533+otr@users.noreply.github.com>
2024-02-03 07:27:06 +0600

    [FEAT] Implement NetworkJson

--- java/books/design_hex_arch/ch04/src/main/java/hexagonal/architecture/framework/adapter/out/file/json/NetworkJson.java
+++ java/books/design_hex_arch/ch04/src/main/java/hexagonal/architecture/framework/adapter/out/file/json/NetworkJson.java
@@ -1,4 +1,25 @@
 package hexagonal.architecture.framework.adapter.out.file.json;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
 public class NetworkJson {
+
+    @JsonProperty("ip")
+    IPJson ip;
+
+    @JsonProperty("networkName")
+    String networkName;
+
+    @JsonProperty("networkCidr")
+    String cidr;
+
 }
