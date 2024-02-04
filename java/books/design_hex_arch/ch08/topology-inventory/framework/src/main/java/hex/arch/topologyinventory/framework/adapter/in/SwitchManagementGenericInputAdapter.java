b5b937e7ac83fda9e9193aeed358bbba82b5711c
OTR <49165533+otr@users.noreply.github.com>
2024-02-04 14:40:25 +0600

    [FEAT] Implement Switch Input Adapter

--- java/books/design_hex_arch/ch08/topology-inventory/framework/src/main/java/hex/arch/topologyinventory/framework/adapter/in/SwitchManagementGenericInputAdapter.java
+++ java/books/design_hex_arch/ch08/topology-inventory/framework/src/main/java/hex/arch/topologyinventory/framework/adapter/in/SwitchManagementGenericInputAdapter.java
@@ -5,11 +5,22 @@ import hex.arch.topologyinventory.application.port.in.SwitchManagementInputPort;
 import hex.arch.topologyinventory.application.use_case.RouterManagementUseCase;
 import hex.arch.topologyinventory.application.use_case.SwitchManagementUseCase;
 
+import hex.arch.topologyinventory.domain.entity.EdgeRouter;
+import hex.arch.topologyinventory.domain.entity.Router;
 import hex.arch.topologyinventory.domain.entity.Switch;
-import hex.arch.topologyinventory.domain.vo.Id;
 
-import hex.arch.topologyinventory.framework.adapter.out.h2.RouterManagementH2OutputAdapter;
-import hex.arch.topologyinventory.framework.adapter.out.h2.SwitchManagementH2OutputAdapter;
+import hex.arch.topologyinventory.domain.vo.IP;
+import hex.arch.topologyinventory.domain.vo.Id;
+import hex.arch.topologyinventory.domain.vo.Location;
+import hex.arch.topologyinventory.domain.vo.Model;
+import hex.arch.topologyinventory.domain.vo.RouterType;
+import hex.arch.topologyinventory.domain.vo.SwitchType;
+import hex.arch.topologyinventory.domain.vo.Vendor;
+
+import hex.arch.topologyinventory.framework.adapter.out.h2
+    .RouterManagementH2OutputAdapter;
+import hex.arch.topologyinventory.framework.adapter.out.h2
+    .SwitchManagementH2OutputAdapter;
 
 public class SwitchManagementGenericInputAdapter {
 
@@ -25,6 +36,46 @@ public class SwitchManagementGenericInputAdapter {
      */
     public Switch retrieveSwitch(Id switchId) {
         return switchUseCase.retrieveSwitch(switchId);
+    }
+
+    /**
+     * POST /switch/create
+     */
+    public EdgeRouter createAndAddSwitchToEdgeRouter(
+        Vendor vendor, Model model, IP ip, Location location,
+        SwitchType switchType, Id routerId
+    ) {
+        Switch newSwitch = switchUseCase.createSwitch(
+            vendor, model, ip, location, switchType
+        );
+        Router edgeRouter = routerUseCase.retrieveRouter(routerId);
+
+        if (!edgeRouter.getRouterType().equals(RouterType.EDGE)) {
+            throw new UnsupportedOperationException(
+                "Please inform the Id of an Edge router to add a switch"
+            );
+        }
+
+        Router router = switchUseCase.addSwitchToEdgeRouter(
+            newSwitch, (EdgeRouter) edgeRouter
+        );
+
+        return (EdgeRouter) routerUseCase.persistRouter(router);
+    }
+
+    /**
+     * POST /switch/remove
+     */
+    public EdgeRouter removeSwitchFromEdgeRouter(
+        Id switchId, Id edgeRouterId
+    ) {
+        EdgeRouter edgeRouter = (EdgeRouter) routerUseCase
+            .retrieveRouter(edgeRouterId);
+        Switch networkSwitch = edgeRouter.getSwitches().get(switchId);
+        Router router = switchUseCase.removeSwitchFromEdgeRouter(
+            networkSwitch, edgeRouter
+        );
+        return (EdgeRouter) routerUseCase.persistRouter(router);
     }
 
     private void setPorts() {
