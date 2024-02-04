fda6fa347681424134108dbc6ec579b220331f00
OTR <49165533+otr@users.noreply.github.com>
2024-02-04 08:31:45 +0600

    [TEST] Add steps for remaining Cucumber features

--- java/books/design_hex_arch/ch07/topology-inventory/application/src/test/java/hex/arch/topologyinventory/application/RouterAdd.java
+++ java/books/design_hex_arch/ch07/topology-inventory/application/src/test/java/hex/arch/topologyinventory/application/RouterAdd.java
@@ -1,22 +1,26 @@
 package hex.arch.topologyinventory.application;
 
-import hex.arch.topologyinventory.application.use_case.RouterManagementUseCase;
 import hex.arch.topologyinventory.domain.entity.CoreRouter;
 import hex.arch.topologyinventory.domain.entity.EdgeRouter;
 import hex.arch.topologyinventory.domain.vo.IP;
-import hex.arch.topologyinventory.domain.vo.Location;
 import hex.arch.topologyinventory.domain.vo.Model;
 import hex.arch.topologyinventory.domain.vo.RouterType;
 import hex.arch.topologyinventory.domain.vo.Vendor;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-public class RouterAdd {
+public class RouterAdd extends ApplicationTestData {
 
-    private RouterManagementUseCase routerManagementUseCase;
-    private Location locationA;
+    CoreRouter anotherCoreRouter;
+
+    public RouterAdd() {
+        loadData();
+    }
 
     @Given("I have an edge router")
     public void assert_edge_router_exists() {
@@ -46,6 +50,51 @@ public class RouterAdd {
             );
 
         assertNotNull(coreRouter);
+    }
+
+
+    @Then("I add an edge router to a core router")
+    public void add_edge_to_core_router() {
+        var actualEdgeId = edgeRouter.getId();
+        var routerWithEdge = (CoreRouter) this.routerManagementUseCase.
+            addRouterToCoreRouter(edgeRouter, coreRouter);
+        var expectedEdgeId = routerWithEdge.getRouters().get(actualEdgeId).getId();
+        assertEquals(actualEdgeId, expectedEdgeId);
+    }
+
+    //Adding a core router to another core router
+    @Given("I have this core router")
+    public void assert_this_core_router_exists() {
+        coreRouter = (CoreRouter) this.routerManagementUseCase.createRouter(
+            Vendor.CISCO,
+            Model.XYZ0001,
+            IP.fromAddress("30.0.0.1"),
+            locationA,
+            RouterType.CORE
+        );
+        assertNotNull(coreRouter);
+    }
+
+    @And("I have another core router")
+    public void assert_another_core_router_exists() {
+        anotherCoreRouter = (CoreRouter) this.routerManagementUseCase.createRouter(
+            Vendor.CISCO,
+            Model.XYZ0001,
+            IP.fromAddress("40.0.0.1"),
+            locationA,
+            RouterType.CORE
+        );
+        assertNotNull(anotherCoreRouter);
+    }
+
+    @Then("I add a core router to another core router")
+    public void add_core_to_core_router() {
+        var coreRouterId = coreRouter.getId();
+        var routerWithCore = (CoreRouter) this.routerManagementUseCase.
+            addRouterToCoreRouter(coreRouter, anotherCoreRouter);
+        var expectedCoreId = routerWithCore.
+            getRouters().get(coreRouterId).getId();
+        assertEquals(coreRouterId, expectedCoreId);
     }
 
 }
