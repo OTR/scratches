3aa91ce447f67d394735c5a4778fc97a6c1fbb4b
OTR <49165533+otr@users.noreply.github.com>
2024-02-06 10:01:11 +0600

    [FEAT] Implement Json DTO for Slug domain entity

--- java/slug_generator/framework/src/main/java/otr/slug/framework/adapter/out/file/SlugManagementFileOutputAdapter.java
+++ java/slug_generator/framework/src/main/java/otr/slug/framework/adapter/out/file/SlugManagementFileOutputAdapter.java
@@ -1,6 +1,8 @@
 package otr.slug.framework.adapter.out.file;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import otr.slug.application.port.out.SlugManagementOutputPort;
 import otr.slug.domain.vo.Slug;
 import otr.slug.framework.adapter.out.file.json.SlugJson;
@@ -8,25 +10,35 @@ import otr.slug.framework.adapter.out.file.mapper.SlugJsonFileMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.UUID;
 
-public class SlugManagementFileOutputAdapter implements SlugManagementOutputPort {
+public class SlugManagementFileOutputAdapter
+    implements SlugManagementOutputPort {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper;
+    private List<SlugJson> slugs;
+    private InputStream resource;
+
+    private SlugManagementFileOutputAdapter() {
+        this.objectMapper = new ObjectMapper();
+        this.resource = getClass().getClassLoader()
+            .getResourceAsStream("slugs.json");
+        readJsonFile();
+    }
 
     @Override
     public Slug fetchSlugById(UUID slugId) {
-        Slug slug;
 
         for (SlugJson slugJson : slugs) {
             if (slugJson.getSlugId().equals(slugId)) {
-                slug = SlugJsonFileMapper.toDomain(slugJson);
-                break;
+                return SlugJsonFileMapper.toDomain(slugJson);
             }
         }
 
-        return slug;
+        throw new IllegalArgumentException("There is no Slug with such Id");
     }
 
     @Override
@@ -44,4 +56,16 @@ public class SlugManagementFileOutputAdapter implements SlugManagementOutputPort
 
         return true;
     }
+
+    private void readJsonFile() {
+        try {
+            this.slugs = objectMapper.readValue(
+                this.resource,
+                new TypeReference<List<SlugJson>>() {}
+            );
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- java/slug_generator/framework/src/main/java/otr/slug/framework/adapter/out/file/json/SlugJson.java
+++ java/slug_generator/framework/src/main/java/otr/slug/framework/adapter/out/file/json/SlugJson.java
@@ -1,11 +1,25 @@
 package otr.slug.framework.adapter.out.file.json;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.UUID;
 
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
 public class SlugJson {
 
+    @JsonProperty(value = "slug_id")
+    private UUID slugId;
+
+    @JsonProperty(value = "slug_value")
+    private String slugValue;
+
     public UUID getSlugId() {
-        return null;
+        return slugId;
+    }
+
+    public String getSlugValue() {
+        return slugValue;
     }
 
 }
